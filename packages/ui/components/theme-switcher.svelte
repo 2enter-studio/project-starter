093fb930e210cca9bb2f8d839c3f72158462e630
davilivob <david.iii.chiu@gmail.com>
2024-10-11 18:48:40 +0800

    [ui] update theme switcher

--- packages/ui/components/theme-switcher.svelte
+++ packages/ui/components/theme-switcher.svelte
@@ -2,30 +2,35 @@
 	import { onMount } from 'svelte';
 	import { themeChange } from 'theme-change';
 	import { capitalize } from '@repo/lib/utils/calc';
-	import tailwindConfig from '@repo/config/tailwind.config.js';
+	import { THEMES } from '@repo/config/tailwind.config';
+	import type { ThemeName } from '@repo/config/tailwind.config';
 
 	interface Props {
-		options?: string[];
+		ignoreThemes?: ThemeName[];
 		open?: boolean;
+		theme?: ThemeName;
 	}
 
-	const defaultOptions = tailwindConfig.daisyui.themes.map((item) => {
-		return typeof item === 'string' ? item : Object.keys(item)[0];
-		// return item.name;
+	let { open = false, theme, ignoreThemes = [] }: Props = $props();
+
+	$effect(() => {
+		// if (!document) return;
+		for (const mode of ['light', 'dark']) {
+			if (THEMES[mode].includes(theme)) document.documentElement.className = mode;
+		}
 	});
+	const options = [...THEMES.light, ...THEMES.dark].filter((t) => !ignoreThemes.includes(t));
 
-	let { options = defaultOptions, open = false }: Props = $props();
-	let currentTheme = $state('Theme');
-
-	function switchTheme(option: string) {
+	function switchTheme(option: ThemeName) {
 		// open = false;
-		currentTheme = option;
+		theme = option;
 	}
 
 	onMount(() => {
+		let initTheme = localStorage.getItem('theme') as ThemeName;
+		if (!options.includes(initTheme)) initTheme = 'retro';
+		switchTheme(initTheme);
 		themeChange(false);
-		if (document) currentTheme = document.body.getAttribute('data-theme');
-		console.log(currentTheme);
 	});
 </script>
 
@@ -42,7 +47,7 @@
 	</summary>
 	<div class="menu dropdown-content rounded-box bg-primary text-primary-content btn-group-vertical z-[1] flex flex-col gap-2 px-2">
 		{#each options as option}
-			{@const classes = option === currentTheme ? 'bg-secondary shadow-inner shadow-black/30 pointer-events-none' : ''}
+			{@const classes = option === theme ? 'bg-secondary shadow-inner shadow-black/30 pointer-events-none' : ''}
 			<button data-set-theme={option} data-act-class="ACTIVECLASS" class="rounded-xl px-1 {classes}" onclick={() => switchTheme(option)}>
 				{capitalize(option)}
 			</button>
