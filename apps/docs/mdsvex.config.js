505029b2d39e8bad77dd53f55550c87cad8541f0
davilivob <david.iii.chiu@gmail.com>
2024-10-13 16:20:02 +0800

    [docs] optimize mdsvex.config.ts --> mdsvex.config.js

--- apps/docs/mdsvex.config.js
+++ apps/docs/mdsvex.config.js
@@ -1,5 +1,5 @@
 import { defineMDSveXConfig as defineConfig, escapeSvelte } from 'mdsvex';
-import type { MdsvexOptions } from 'mdsvex';
+// import type { MdsvexOptions } from 'mdsvex';
 import remarkUnwrapImages from 'remark-unwrap-images';
 import remarkCodeTitle from 'remark-code-title';
 
@@ -8,16 +8,17 @@ import rehypeToc from 'rehype-toc';
 
 import { createHighlighter } from 'shiki';
 
-const themes = ['tokyo-night', 'catppuccin-latte'];
+const themes = { dark: 'tokyo-night', light: 'catppuccin-latte' };
+
 // @ts-ignore
 const highlighter = await createHighlighter({
-	themes,
+	themes: Object.values(themes),
 	langs: ['javascript', 'typescript', 'svelte'],
 	langAlias: { sveltehtml: 'svelte' }
 });
 
 /** @type {import('mdsvex').MdsvexOptions} */
-const config: MdsvexOptions = {
+const config = {
 	extensions: ['.md'],
 	smartypants: {
 		dashes: 'oldschool'
@@ -27,7 +28,7 @@ const config: MdsvexOptions = {
 			const html = escapeSvelte(
 				highlighter.codeToHtml(code, {
 					lang,
-					themes: { light: themes[1], dark: themes[0] }
+					themes
 				})
 			);
 			return `{@html \`${html}\` }`;
@@ -41,7 +42,7 @@ const config: MdsvexOptions = {
 			{
 				cssClasses: {
 					nav: false,
-					toc: 'text-xs overflow-y-auto max-h-72 backdrop-blur-sm bg-base-300/60 shadow-lg shadow-black/30 rounded-lg py-2 px-4 w-fit fixed top-3 right-3',
+					toc: 'text-xs hidden overflow-y-auto max-h-72 backdrop-blur-sm bg-base-300/60 shadow-lg shadow-black/30 rounded-lg py-2 px-4 w-fit',
 					list: 'my-0 list-disc',
 					link: 'link link-primary'
 				}
