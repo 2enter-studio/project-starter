c29833fbccbeec42685b240e8730a43279aa80d1
davilivob <david.iii.chiu@gmail.com>
2024-10-13 17:04:51 +0800

    [docs] replace placeholder for root page

--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
 </script>
 
-<h1 class="center-content text-secondary">Hello fuck you</h1>
-<h1 class="center-content text-accent">Hello fuck you</h1>
-<h1 class="center-content text-neutral">Hello fuck you</h1>
+<h1 class="center-content text-secondary">Hi hi hi</h1>
