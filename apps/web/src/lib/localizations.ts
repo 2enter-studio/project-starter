78fee6d426c8005366d6525729fd07c4fac96c28
davilivob <david.iii.chiu@gmail.com>
2024-10-26 17:17:51 +0800

    [web] more example code for localizations

--- apps/web/src/lib/localizations.ts
+++ apps/web/src/lib/localizations.ts
@@ -1,12 +1,13 @@
-type Keys = 'hello' | 'world';
 const LOCALES = ['zh-tw', 'en'] as const;
 const DEFAULT_LOCALE = LOCALES[0];
 type Locale = (typeof LOCALES)[number];
 
+type Keys = 'hello' | 'world' | 'locale' | 'shortenLocale';
+
 const localizations: Record<Locale, Record<Keys, string>> = {
-	'zh-tw': { hello: '', world: '' },
-	en: { hello: '', world: '' }
-} as const;
+	'zh-tw': { locale: '繁體中文', shortenLocale: '繁中', hello: '你好', world: '世界' },
+	en: { locale: 'English', shortenLocale: 'EN', hello: 'hello', world: 'world' }
+};
 
 export { localizations, LOCALES, DEFAULT_LOCALE };
 export type { Locale };
