236115f127bbeed0176a49841b98a3592cd32cab
davilivob <david.iii.chiu@gmail.com>
2024-10-26 21:08:44 +0800

    [web] use eruda for developing

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { onMount } from 'svelte';
+
 	import { PUBLIC_FA_KIT_URL } from '$env/static/public';
 	import '@repo/config/app.css';
 
 	let { children } = $props();
+	onMount(async () => {
+		if (dev) {
+			const eruda = (await import('eruda')).default;
+			eruda.init();
+		}
+	});
 </script>
 
 <svelte:head>
