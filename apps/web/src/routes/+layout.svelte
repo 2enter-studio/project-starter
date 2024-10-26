7ca7ef223ad4816e24b03acf3906dcda73dbcbbe
davilivob <david.iii.chiu@gmail.com>
2024-10-26 20:47:36 +0800

    [web] put more useful code in +layout.svelte

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { PUBLIC_FA_KIT_URL } from '$env/static/public';
 	import '@repo/config/app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#if PUBLIC_FA_KIT_URL}
+		<script src={PUBLIC_FA_KIT_URL} crossorigin="anonymous"></script>
+	{/if}
+</svelte:head>
 
 {@render children()}
