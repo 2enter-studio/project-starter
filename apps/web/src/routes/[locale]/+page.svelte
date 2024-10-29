d9234956d790bc267f55aab510e3673f5835044d
davilivob <david.iii.chiu@gmail.com>
2024-10-29 15:30:44 +0800

    [web] set sysState.locale in /[locale]/+page.svelte

--- apps/web/src/routes/[locale]/+page.svelte
+++ apps/web/src/routes/[locale]/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { sysState } from '@/states';
+
 	let { data } = $props();
-	const { locale, localization } = data;
+	const { locale } = data;
+
+	onMount(() => {
+		sysState.locale = locale;
+	});
 </script>
 
 {locale}
