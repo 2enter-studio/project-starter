754fb994952a96be8718d98a5afd4793782f123c
davilivob <david.iii.chiu@gmail.com>
2024-11-13 20:24:16 +0800

    [web] rename pages & put example codes

--- apps/web/src/routes/[locale]/pages/index.ts
+++ apps/web/src/routes/[locale]/pages/index.ts
@@ -1,5 +1,5 @@
-import { default as P0 } from './0.svelte';
-import { default as P1 } from './1.svelte';
-import { default as P2 } from './2.svelte';
+import { default as Intro } from './intro.svelte';
+import { default as Name } from './name.svelte';
+import { default as Message } from './message.svelte';
 
-export const Pages = [P0, P1, P2];
+export const Pages = [Intro, Name, Message];
