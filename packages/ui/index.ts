13e6a9421bccba9061d0103545ec20bf4aeec18e
davilivob <david.iii.chiu@gmail.com>
2024-11-22 15:05:00 +0800

    [@repo/ui] add component <FullscreenChecker>

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as ThemeSwitcher } from './components/theme-switcher.svelte';
 export { default as Marquee } from './components/marquee.svelte';
+export { default as FullscreenChecker } from './components/fullscreen_checker.svelte';
