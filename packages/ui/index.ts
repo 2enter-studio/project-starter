18351e49f91d4e098aca5bc50dc6743dd593e992
davilivob <david.iii.chiu@gmail.com>
2024-11-22 17:01:05 +0800

    [@repo/ui] create component <ImgBtn>

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as ThemeSwitcher } from './components/theme-switcher.svelte';
 export { default as Marquee } from './components/marquee.svelte';
 export { default as FullscreenChecker } from './components/fullscreen_checker.svelte';
+export { default as ImgBtn } from './components/img_btn.svelte';
