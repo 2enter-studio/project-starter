86b21ab078763f3e1e47b3e8bfd2882d7ae15d78
davilivob <david.iii.chiu@gmail.com>
2024-12-06 18:13:28 +0800

    [@repo/ui] create new component: <TreeView>

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,3 +2,4 @@ export { default as ThemeSwitcher } from './components/theme_switcher.svelte';
 export { default as Marquee } from './components/marquee.svelte';
 export { default as FullscreenChecker } from './components/fullscreen_checker.svelte';
 export { default as ImgBtn } from './components/img_btn.svelte';
+export { default as TreeView } from './components/treeview.svelte';
